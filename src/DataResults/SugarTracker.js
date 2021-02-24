e461d8e51bedc2d6ae992a148edd985eef7cee42
Audra <73372336+IamAudra@users.noreply.github.com>
2021-02-24 15:42:43 -0500

    Create Data Results Pages

---
+++ src/DataResults/SugarTracker.js
@@ -0,0 +1,15 @@
+import {Footer, NavBar} from "../../Components";
+
+export const SugarTracker = () => (
+    <>
+        <NavBar/>
+        <div className="p-10 bg-blue-600" >
+            Sugar Tracker
+            Input Food Data
+            ESHA API to get data
+        </div>
+        <Footer/>
+    </>
+
+
+    )
